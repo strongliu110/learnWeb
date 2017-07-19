5a8018990f243b463719c1683f58c6a104f9e541
liuyuzhe <liuyuzhe110@qq.com>
2017-07-19 23:01:17 +0800

    aliyun 一级导航基本动画效果实现

--- aliyun/js/index.js
+++ aliyun/js/index.js
@@ -3,17 +3,43 @@ $(".icon-wrong-thin").on("click", function() {
 });
 
 $(".product-show-more").on("click", function() {
-	var more = $(".project-more");
-	more.is(":hidden") ? (
-		$(this).text("查看全部")
-	) : (
+	var $more = $(".project-more");
+	$more.is(":hidden") ? (
 		$(this).text("收起")
+	) : (
+		$(this).text("查看全部")
 	);
 
-	more.animate({
+	$more.animate({
 		opacity: "toggle",
 		height: "toggle"
 	}, 300, "linear");
 });
 
-$(".common-topbar-nav-list").children(".line") 
+$(".common-topbar-nav-list").on("mousemove", "li", function(ev) {
+	var $line = $(".common-topbar-nav-list").find(".line");
+	$line.css({
+		width: $(this).outerWidth(),
+		left: $(this).position().left
+	});
+});
+
+$(".common-topbar-nav-list").on("mouseleave", function() {
+	var $line = $(".common-topbar-nav-list").find(".line");
+	$line.css({
+		width: 0
+	});
+});
+
+var $nav = $(".common-topbar-all-nav-dropdown");
+var $level1 = $(".common-topbar-level1-nav");
+$(".all-nav").on("mouseenter", function() {
+	$level1.animate({
+		left: 0
+	}, 300, "linear");
+});
+$(".all-nav").on("mouseleave", function() {
+	$level1.animate({
+		left: -200
+	}, 200, "linear");
+});
